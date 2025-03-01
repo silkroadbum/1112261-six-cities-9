623d6e8b1f8ca7e3597f511bc9bba6c377cdcce8
Egorov Anatolii <silkroadbum@mail.ru>
2025-03-01 20:35:13 +0300

    add mock server data type

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,16 @@
+import { Coordinates } from './coordinates.type.js';
+
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  photos: string[];
+  houseTypes: string[];
+  amenities: string[];
+  userNames: string[];
+  emails: string[];
+  avatars: string[];
+  userTypes: string[];
+  coordinates: Coordinates[];
+};
